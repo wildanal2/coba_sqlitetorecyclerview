eb069b4e3902a534bd0e7a5eca2a531148564ccc
wildanal2PC <40136374+wildanal2@users.noreply.github.com>
2018-10-18 23:51:25 +0700

    fixed add
    add cmd

---
+++ app/src/main/java/com/sqliterecycler/com/sqlite_recycler/ItemAdapter.java
@@ -0,0 +1,67 @@
+package com.sqliterecycler.com.sqlite_recycler;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ItemAdapter  extends RecyclerView.Adapter<ItemAdapter.ItemsViewHolder>{
+    private Context mContext;
+    private Cursor mCursor;
+
+    public ItemAdapter(Context contx, Cursor crsor) {
+        mContext = contx;
+        mCursor = crsor;
+    }
+
+    public class ItemsViewHolder extends RecyclerView.ViewHolder{
+        TextView nameText,jumlahtext;
+
+        public ItemsViewHolder(View itemView) {
+            super(itemView);
+            nameText = itemView.findViewById(R.id.txv_namaBarang);
+            jumlahtext = itemView.findViewById(R.id.txv_jumlahBarang);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ItemsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inf = LayoutInflater.from(mContext);
+        View v = inf.inflate(R.layout.item_list,parent,false);
+        return new ItemsViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsViewHolder holder, int position) {
+        if (!mCursor.moveToPosition(position)){
+            return;
+        }
+
+        String name = mCursor.getString(mCursor.getColumnIndex("nama"));
+        int amount = mCursor.getInt(mCursor.getColumnIndex("jumlah"));
+
+        holder.nameText.setText(name);
+        holder.jumlahtext.setText(String.valueOf(amount));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor.getCount();
+    }
+
+    public  void  swapCursor(Cursor newCursor){
+        if (mCursor != null){
+            mCursor.close();
+        }
+
+        mCursor = newCursor;
+        if (newCursor != null){
+            notifyDataSetChanged();
+        }
+    }
+}
--- app/src/main/java/com/sqliterecycler/com/sqlite_recycler/MainActivity.java
+++ app/src/main/java/com/sqliterecycler/com/sqlite_recycler/MainActivity.java
@@ -1,21 +1,103 @@
 package com.sqliterecycler.com.sqlite_recycler;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ItemAdapter mAdapter;
+    private SQLiteDatabase mDatabase;
     private EditText mETname;
     private TextView mTVamount;
-    private int mAmount = 0;
+    private int mAmount = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mETname
+        ItemDbHelper dbHelper = new ItemDbHelper(this);
+        mDatabase = dbHelper.getWritableDatabase();
+
+        RecyclerView recyView = findViewById(R.id.recy_view);
+        recyView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new ItemAdapter(this,getAllItems());
+        recyView.setAdapter(mAdapter);
+
+        mETname = findViewById(R.id.et_named);
+        mTVamount = findViewById(R.id.tv_counter);
+        Button btnadd = findViewById(R.id.btn_add);
+        Button btnIncrease = findViewById(R.id.btn_plus);
+        Button btnDecrease = findViewById(R.id.btn_min);
+
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Increast();
+            }
+        });
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrease();
+            }
+        });
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItems();
+            }
+        });
+    }
+
+    private void addItems() {
+
+        if(mETname.getText().toString().trim().length() ==0 || mAmount==0){
+            return;
+        }
+
+        String name = mETname.getText().toString();
+        ContentValues  val = new ContentValues();
+        val.put("nama", name);
+        val.put("jumlah", mAmount);
+
+        mDatabase.insert("belanjaan",null,val);
+        Toast.makeText(getApplicationContext(),"Sukses Ditambahkan ",Toast.LENGTH_SHORT).show();
+        mAdapter.swapCursor(getAllItems());
+
+        mETname.setText("");
+        mAmount=0;
+        mTVamount.setText("0");
+    }
+
+    private void decrease() {
+        if (mAmount>0){
+            mAmount--;
+            mTVamount.setText(String.valueOf(mAmount));
+        }
+    }
+
+    private void Increast() {
+        mAmount++;
+        mTVamount.setText(String.valueOf(mAmount));
+    }
+
+    private Cursor getAllItems(){
+        return mDatabase.query("belanjaan",
+                null,
+                null,
+                null,
+                null,
+                null,
+                "timestmp DESC");
     }
 }
